b9e04a01258ed1eca7abc7408099a84aaf93ef0b
Gauri Bane <sunitagamne16@gmail.com>
2022-01-23 23:14:45 +0530

    merged changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import "./App.css";
 import RoutingService from "./services/RoutingService";
-import {store} from "../src/app/store"
+import {store} from "../src/app/store";
+import { sagaMiddleware } from './app/store';
+import rootSaga from "./app/api/rootSaga";
+sagaMiddleware.run(rootSaga)
 
 let persistor = persistStore(store);
 
