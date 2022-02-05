201dd757c42505dbba7c08d6fa38c1f4f78fa2b9
Gauri Bane <sunitagamne16@gmail.com>
2022-02-05 18:41:37 +0530

    minor change

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import config from "../../config.json"
 const Axios = axios.create({
-  baseURL: config.result.envDetails.REACT_APP_API_LOCAL_URL,
+  baseURL: config.result.envDetails.REACT_APP_API_URL,
 });
 export default Axios;
